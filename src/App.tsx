c4537c61d3965db40b5e22e8dc1e74ebe71b02d3
Ovramenko Andrii <anovramenko2@gmail.com>
2025-03-20 15:11:53 +0200

    fix some issues

--- src/App.tsx
+++ src/App.tsx
@@ -22,15 +22,15 @@ export class App extends React.Component<State> {
   }
 
   render(): React.ReactNode {
+    const { pressedKey } = this.state;
+
     return (
       <div className="App">
-        {this.state.pressedKey ? (
-          <p className="App__message">
-            {`The last pressed key is [${this.state.pressedKey}]`}
-          </p>
-        ) : (
-          <p className="App__message">Nothing was pressed yet</p>
-        )}
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
       </div>
     );
   }
